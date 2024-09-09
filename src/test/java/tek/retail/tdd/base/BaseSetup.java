3e3cc373d6848acf501c81773f6459d9680626bc
Morteza <morteza.sharifi.1995@gmail.com>
2024-09-09 15:49:30 -0400

    organized the imports

--- src/test/java/tek/retail/tdd/base/BaseSetup.java
+++ src/test/java/tek/retail/tdd/base/BaseSetup.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
